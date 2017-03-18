150968fbc45fca228ba6562c7608307e221cf817
vahvarenc <vahagn.vardanyan@gmail.com>
2017-03-18 18:41:09 +0400

    added 1st HW

---
+++ src/homeworks/Triangle.java
@@ -0,0 +1,15 @@
+package homeworks;
+
+/**
+ * Created by Karen on 3/18/2017.
+ */
+public class Triangle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
