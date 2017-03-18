e51126095ffadbf74abafdea7904102230f50b99
Vahag <vahagn.vardanyan@gmail.com>
2017-03-18 19:08:32 +0400

    changes in the 1st HW

--- src/homeworks/DrawTriangle.java
+++ src/homeworks/DrawTriangle.java
@@ -3,13 +3,17 @@ package homeworks;
 /**
  * Created by Karen on 3/18/2017.
  */
-public class Triangle {
-    public static void main(String[] args) {
+public class DrawTriangle {
+    public static void drawTriangle(int n){
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        drawTriangle(10);
     }
 }
